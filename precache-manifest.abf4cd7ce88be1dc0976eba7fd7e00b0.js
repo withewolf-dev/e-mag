3aef466005c0f45469440a80c84da7f86e685efb
withewolf-dev <itskashyap26@gmail.com>
2020-07-05 19:51:16 +0700

    Updates

---
+++ precache-manifest.abf4cd7ce88be1dc0976eba7fd7e00b0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a812817df91c60a7dadf16add73c96d4",
+    "url": "/e-mag/index.html"
+  },
+  {
+    "revision": "7d9c50b583c782e05eb2",
+    "url": "/e-mag/static/css/main.624a178d.chunk.css"
+  },
+  {
+    "revision": "1ed70d1a189861778809",
+    "url": "/e-mag/static/js/0.22bbcd6b.chunk.js"
+  },
+  {
+    "revision": "04281d2a6a9321b3d2d2",
+    "url": "/e-mag/static/js/1.a9e17190.chunk.js"
+  },
+  {
+    "revision": "692688fa83c1779aab8cd445ec046a8a",
+    "url": "/e-mag/static/js/1.a9e17190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3bd2ff63b5de69e8a64",
+    "url": "/e-mag/static/js/10.f9f95ed3.chunk.js"
+  },
+  {
+    "revision": "65d7474db4cff692b230",
+    "url": "/e-mag/static/js/11.381bb338.chunk.js"
+  },
+  {
+    "revision": "f64870ddef39e723d48e",
+    "url": "/e-mag/static/js/12.8903ef49.chunk.js"
+  },
+  {
+    "revision": "c36d3f4974a231a68122",
+    "url": "/e-mag/static/js/4.00734e3f.chunk.js"
+  },
+  {
+    "revision": "b523c101065cfd09f2871176e28e1ba9",
+    "url": "/e-mag/static/js/4.00734e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f06314c7b4ab0b0527d5",
+    "url": "/e-mag/static/js/5.c1eee38f.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/e-mag/static/js/5.c1eee38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8ae1e8032575a801f7",
+    "url": "/e-mag/static/js/6.07dc4a66.chunk.js"
+  },
+  {
+    "revision": "43e60c207f9704ef9c28",
+    "url": "/e-mag/static/js/7.90aac360.chunk.js"
+  },
+  {
+    "revision": "eb39ad53df652dbc90fc",
+    "url": "/e-mag/static/js/8.e9ee0aaf.chunk.js"
+  },
+  {
+    "revision": "c59ad807759acef3c8f2",
+    "url": "/e-mag/static/js/9.32e84db4.chunk.js"
+  },
+  {
+    "revision": "7d9c50b583c782e05eb2",
+    "url": "/e-mag/static/js/main.87e94aed.chunk.js"
+  },
+  {
+    "revision": "c372f898e11f5a3ae447",
+    "url": "/e-mag/static/js/runtime-main.b38e2a8d.js"
+  }
+]);
